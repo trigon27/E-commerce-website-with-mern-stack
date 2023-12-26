a76e4ff2afcb9f31901068bf025a67fd64f1fe46
trigon27 <ashrafshaikh0778866@gmail.com>
2023-12-26 21:22:23 +0530

    initial files and create product list

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Product from "./features/ProductList/ProductList";
+
+function App() {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+
+export default App;
