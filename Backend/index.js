b90e7e247c4037f30900d5c6fbde8e6bf3686f32
trigon27 <ashrafshaikh0778866@gmail.com>
2024-01-14 19:10:26 +0530

    Added passport and jwt token in backend

--- Backend/index.js
+++ Backend/index.js
@@ -9,26 +9,127 @@ const UserRouter = require("./routes/User");
 const AuthRouter = require("./routes/Auth");
 const CartRouter = require("./routes/Cart");
 const OrderRouter = require("./routes/Order");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const cors = require("cors");
-//middleware
-server.use(express.json());
+const session = require("express-session");
+const passport = require("passport");
+
+const { User } = require("./model/User");
+const { isAuth, sanitizeUser } = require("./services/common");
+const LocalStrategy = require("passport-local").Strategy;
+
+const SECRET_KEY = "SECRET_KEY";
+// JWT options
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
+
+// Middleware
+server.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Initialize Passport
+server.use(passport.initialize());
+server.use(passport.session());
+
 server.use(
   cors({
     exposedHeaders: ["X-Total-Count"],
   })
 );
+
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.json({ Status: "success" });
 });
 
-server.use("/products", productsRouter.router);
-server.use("/brands", brandsRouter.router);
-server.use("/categories", categoryRouter.router);
-server.use("/users", UserRouter.router);
+server.use("/products", isAuth(), productsRouter.router);
+server.use("/brands", isAuth(), brandsRouter.router);
+server.use("/categories", isAuth(), categoryRouter.router);
+server.use("/users", isAuth(), UserRouter.router);
 server.use("/auth", AuthRouter.router);
-server.use("/Cart", CartRouter.router);
-server.use("/orders", OrderRouter.router);
-//database connection
+server.use("/Cart", isAuth(), CartRouter.router);
+server.use("/orders", isAuth(), OrderRouter.router);
+
+// Passport strategy
+passport.use(
+  "local",
+
+  new LocalStrategy({ usernameField: "email" }, async function (
+    email,
+    password,
+    done
+  ) {
+    try {
+      const user = await User.findOne({ email: email }).exec();
+      console.log(email, password, user);
+      if (!user) {
+        return done(null, false, { message: "no such user email" });
+      }
+
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        "sha256",
+        async function (err, hashedPassword) {
+          if (
+            !crypto.timingSafeEqual(
+              Buffer.from(user.password, "hex"),
+              hashedPassword
+            )
+          ) {
+            return done(null, false, { message: "invalid credentials" });
+          }
+          const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
+          done(null, token);
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    console.log({ jwt_payload });
+    try {
+      const user = await User.findOne({ id: jwt_payload.sub });
+      if (user) {
+        return done(null, sanitizeUser(user)); // this calls serializer
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+// Database connection
 const connectToMongoDB = async () => {
   try {
     await mongoose.connect(
